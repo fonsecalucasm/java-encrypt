aaffd6a6fc78d9dd3c740f4750e7832a1d4f8351
Lucas M Fonseca <lmdf.013@gmail.com>
2019-12-21 14:15:29 -0300

    First commit

---
+++ src/test/java/com/fonseca/lucas/Encrypt/EncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonseca.lucas.Encrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
